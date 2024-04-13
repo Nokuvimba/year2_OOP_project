32bd87c016f423202dca4c636f377fc71134def8
anesu <g00425238@atu.ie>
2024-04-13 20:05:05 +0100

    Bug fixes in admin password

--- src/main/java/ie/atu/MainApplication.java
+++ src/main/java/ie/atu/MainApplication.java
@@ -14,6 +14,7 @@ public class MainApplication{
         String password = "smartphone";
         int option = 0;
         int adminOption = 0;
+        int passUpdate = 0;
         Scanner scanner = new Scanner(System.in);
         while (option != 3) {
             try {
@@ -56,96 +57,109 @@ public class MainApplication{
                 }
 
                 else if (option == 2) {
-
-                        System.out.println("Please enter password: ");
+                    while (option == 2) {
+                        System.out.println("Please enter password to Enter Admin Menu: ");
                         String passInput = scanner.next();
-                    while (adminOption != 8){
-                        if (Objects.equals(passInput, password)) {
-                            System.out.println("Welcome!");
-                            System.out.println("1. View all smartphones in database");
-                            System.out.println("2. View all customers in database");
-                            System.out.println("3. Add new phone to database");
-                            System.out.println("4. Add new customer to database");
-                            System.out.println("5. Change admin password");
-                            System.out.println("6. Remove customer from database");
-                            System.out.println("7. Update customer info");
-                            System.out.println("8. Exit to Main Menu");
-                            adminOption = scanner.nextInt();
-                            switch (adminOption) {
-                                case 1:
-                                    String data = databaseManagement.getData();
-                                    System.out.println("Data from database: ");
-                                    System.out.println(data);
-                                    break;
-                                case 2:
-                                    String customerData = databaseManagement.getCustomerData();
-                                    System.out.println("Customers from database:");
-                                    System.out.println(customerData);
-                                    break;
-                                case 3:
-                                    System.out.println("Enter phone details.");
-                                    System.out.println("Brand Name: ");
-                                    String brandName = scanner.next();
-                                    System.out.println("Model: ");
-                                    String model = scanner.next();
-                                    System.out.println("cost: ");
-                                    String cost = scanner.next();
-                                    databaseManagement.addDeviceData(brandName, model, cost);
-                                    break;
-                                case 4:
-                                    System.out.println("Enter name: ");
-                                    String name = scanner.next();
-                                    System.out.println("Enter Email: ");
-                                    String email = scanner.next();
-                                    System.out.println("Enter phone number: ");
-                                    String phoneNo = scanner.next();
-                                    databaseManagement.addCustomerData(name, email, phoneNo);
-                                    System.out.println("Customer Added Successfully");
-                                    break;
-                                case 5:
-                                    System.out.println("Enter current password: ");
-                                    passInput = scanner.next();
+                        adminOption = 0;
+                        while (adminOption != 8) {
+                            if (Objects.equals(passInput, password)) {
+                                System.out.println("Welcome!");
+                                System.out.println("1. View all smartphones in database");
+                                System.out.println("2. View all customers in database");
+                                System.out.println("3. Add new phone to database");
+                                System.out.println("4. Add new customer to database");
+                                System.out.println("5. Change admin password");
+                                System.out.println("6. Remove customer from database");
+                                System.out.println("7. Update customer info");
+                                System.out.println("8. Exit to Main Menu");
+                                adminOption = scanner.nextInt();
+                                switch (adminOption) {
+                                    case 1:
+                                        String data = databaseManagement.getData();
+                                        System.out.println("Data from database: ");
+                                        System.out.println(data);
+                                        break;
+                                    case 2:
+                                        String customerData = databaseManagement.getCustomerData();
+                                        System.out.println("Customers from database:");
+                                        System.out.println(customerData);
+                                        break;
+                                    case 3:
+                                        System.out.println("Enter phone details.");
+                                        System.out.println("Brand Name: ");
+                                        String brandName = scanner.next();
+                                        System.out.println("Model: ");
+                                        String model = scanner.next();
+                                        System.out.println("cost: ");
+                                        String cost = scanner.next();
+                                        databaseManagement.addDeviceData(brandName, model, cost);
+                                        break;
+                                    case 4:
+                                        System.out.println("Enter name: ");
+                                        String name = scanner.next();
+                                        System.out.println("Enter Email: ");
+                                        String email = scanner.next();
+                                        System.out.println("Enter phone number: ");
+                                        String phoneNo = scanner.next();
+                                        databaseManagement.addCustomerData(name, email, phoneNo);
+                                        System.out.println("Customer Added Successfully");
+                                        break;
+                                    case 5:
+                                        int passCount = 0;
+                                        while (passCount == 0) {
+                                            System.out.println("Enter current password: ");
+                                            passInput = scanner.next();
 
-                                    if (Objects.equals(passInput, password)) {
-                                        System.out.println("Enter new password: ");
-                                        String newPassword = scanner.next();
+                                            if (Objects.equals(passInput, password)) {
+                                                System.out.println("Enter new password: ");
+                                                String newPassword = scanner.next();
 
-                                        if (Objects.equals(newPassword, password)) {
-                                            while (Objects.equals(newPassword, password)) {
-                                                System.out.println("New password matches the old one");
-                                                System.out.println("Please enter a new password");
-                                                newPassword = scanner.next();
+                                                if (Objects.equals(newPassword, password)) {
+                                                    while (Objects.equals(newPassword, password)) {
+                                                        System.out.println("New password matches the old one");
+                                                        System.out.println("Please enter a new password");
+                                                        newPassword = scanner.next();
+                                                    }
+                                                }
+                                                password = newPassword;
+                                                System.out.println("Password changed! ");
+                                                passCount = 1;
+                                                passUpdate = 1;
+                                                adminOption = 8;
+                                            }
+                                            else {
+                                                System.out.println("Wrong current password!");
                                             }
                                         }
-                                        password = newPassword;
-                                        System.out.println("Password changed! ");
-                                    }
-                                    break;
-                                case 6:
-                                    System.out.println("Enter customer ID of customer to be removed: ");
-                                    int delete = scanner.nextInt();
-                                    databaseManagement.deleteCustomerData(delete);
-                                    break;
-                                case 7:
-                                    System.out.println("Enter customer ID of customer info to be updated: ");
-                                    int custId = scanner.nextInt();
-                                    System.out.println("Enter new email: ");
-                                    String newEmail = scanner.next();
-                                    databaseManagement.updateCustomerData(custId, newEmail);
-                                    break;
-                                case 8:
-                                    System.out.println("Logged out. ");
-                                    break;
-                                default:
+                                        break;
+                                    case 6:
+                                        System.out.println("Enter customer ID of customer to be removed: ");
+                                        int delete = scanner.nextInt();
+                                        databaseManagement.deleteCustomerData(delete);
+                                        break;
+                                    case 7:
+                                        System.out.println("Enter customer ID of customer info to be updated: ");
+                                        int custId = scanner.nextInt();
+                                        System.out.println("Enter new email: ");
+                                        String newEmail = scanner.next();
+                                        databaseManagement.updateCustomerData(custId, newEmail);
+                                        break;
+                                    case 8:
+                                        System.out.println("Logged out. ");
+                                        option = 0;
+                                        break;
+                                    default:
+                                        System.out.println("Invalid Entry!\nEnter only numbers on the Menu");
+                                        break;
+                                }
+                            } else {
+                                if(passUpdate == 0) {
+                                    System.out.println("\nIncorrect password!!!\n");
+                                    adminOption = 8;
 
-                                    break;
+                                }
                             }
                         }
-                        else {
-                            System.out.println("Wrong password...\nPlease enter correct password");
-                            adminOption = 3;
-                        }
-
                     }
                 }
                 else if (option == 3){
